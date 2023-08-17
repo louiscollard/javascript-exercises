5386282813261b97da3cd574ff168added12b8e1
Louis Collard <louiscollard2@gmail.com>
2023-08-17 11:30:30 +0200

    add exo05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+	if (typeof numOne !== "number" || typeof numTwo !== "number") return "ERROR";
+	if (numOne < 0 || numTwo < 0) return "ERROR";
+	if (numOne > numTwo) {
+		const temp = numOne;
+		numOne = numTwo;
+		numTwo = temp;
+	}
 
+	let newSum = [];
+	for (let i = 0; i < numTwo; i++) {
+		newSum[i] = numOne + i;
+	}
+	return newSum.reduce((acc, curr) => acc + curr);
 };
 
 // Do not edit below this line
