9e57a1d2acaad7c153c73b34097879b01b1753e5
Louis Collard <louiscollard2@gmail.com>
2023-08-17 11:29:59 +0200

    Add exo02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+	if (number < 0) return "ERROR";
+	let newStr = [];
+	for (let i = 0; i < number; i++) {
+		newStr.push(string);
+	}
+	return newStr.join("");
 };
-
 // Do not edit below this line
 module.exports = repeatString;
