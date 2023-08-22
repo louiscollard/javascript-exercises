d2182fddba7496a7831be828c9c8e0dc5f2fa7e0
Louis Collard <louiscollard2@gmail.com>
2023-08-22 14:39:08 +0200

    Add Test and Exo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+	if (count < 0) return "OOPS";
+	if (count === 0) return 0;
 
+	let fibonacci = [0, 1];
+	for (let i = 2; i <= count; i++) {
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+	}
+
+	let response = fibonacci.slice(-1);
+	return parseInt(response.join(""));
 };
 
 // Do not edit below this line
