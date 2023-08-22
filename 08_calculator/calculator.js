af0365ea22f9a31ac31718f9ce3b050193741bda
Louis Collard <louiscollard2@gmail.com>
2023-08-22 09:52:28 +0200

    Add exo08 and 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+	return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+	return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num === 0) return 1;
+
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	let punctuation = /[\.,?!]/g;
+	let convertToStr = str.toString().toLowerCase();
 
+	let filterReverseStr = convertToStr
+		.replace(punctuation, "")
+		.split(" ")
+		.join("");
+	let reverseStr = filterReverseStr.split("").reverse().join("");
+
+	if (reverseStr === filterReverseStr) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
